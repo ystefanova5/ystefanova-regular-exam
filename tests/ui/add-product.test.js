33acd64f114145f2702b25600b118164b4c6526d
Yordanka Stefanova <y.stefanova@hotmail.com>
2024-04-13 14:27:30 +0300

    fix failing UI tests

--- tests/ui/add-product.test.js
+++ tests/ui/add-product.test.js
@@ -1,8 +1,8 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add products page', async ({ page }) => {
-    await page.goto('http://localhost:8080/add-to-my-cookbook');
-    const form = await page.$('input');
+    await page.goto('http://localhost:8080/add-product');
+    const form = await page.$('form');
     expect(form).toBeTruthy();
   });
   
